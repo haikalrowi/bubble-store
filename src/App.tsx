4561d0c375d115dffd00c0dbece1d432dfc24e73
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-29 20:43:30 +0700

    md:

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { TopBar } from "./components/TopBar";
 function App() {
   return (
     <div className="bg-blue-50">
-      <div className="sticky top-0 z-10 h-12 bg-blue-900">
+      <div
+        className="sticky top-0 z-10 h-12 bg-blue-900 p-4 text-white transition-all
+        md:h-20 md:p-8"
+      >
         <TopBar />
       </div>
       <div className="h-fit min-h-screen">
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,43 @@
+function Categories() {
+  return (
+    <div className="flex items-center gap-2">
+      <span>Categories</span>
+      <svg
+        className="size-4"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+      >
+        <path
+          fillRule="evenodd"
+          d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div className="flex items-center gap-2 rounded-xl bg-white px-4 py-2 text-blue-900">
+      <img
+        className="size-4"
+        src="https://bubblestore.geniebook.com/_nuxt/img/Icon_Search_Default_Hifi_Web.a22c553.svg"
+        alt="Search"
+      />
+      <input
+        className="text-xs outline-none"
+        type="text"
+        placeholder="Search"
+      />
+    </div>
+  );
+}
+
 export function TopBar() {
   return (
-    <div className="flex size-full items-center gap-4 p-4">
+    <div className="flex size-full items-center justify-evenly gap-4">
       <a href="/">
         <img
           className="h-4 w-32"
@@ -8,17 +45,39 @@ export function TopBar() {
           alt="Bubble Store"
         />
       </a>
-      <span className="flex-1" />
+      <span
+        className="flex-1
+        md:hidden"
+      />
       <img
-        className="size-6"
+        className="size-6
+        md:hidden"
         src="https://bubblestore.geniebook.com/_nuxt/img/icon-search-white.765d2d5.svg"
         alt="Search"
       />
+      <div className="max-md:hidden">
+        <Categories />
+      </div>
+      <div className="flex-1 max-md:hidden">
+        <Search />
+      </div>
       <img
         className="size-6"
         src="https://bubblestore.geniebook.com/_nuxt/img/Icon_Top Nav Bar_Cart.f96d8a4.svg"
         alt="Cart"
       />
+      <div className="max-md:hidden">
+        <img
+          className="size-6"
+          src="https://bubblestore.geniebook.com/_nuxt/img/wishlist.3812699.svg"
+          alt="Wishlist"
+        />
+      </div>
+      <div className="max-md:hidden">
+        <button className="rounded-full bg-white px-4 py-2 text-xs font-bold text-blue-900">
+          Login
+        </button>
+      </div>
     </div>
   );
 }
