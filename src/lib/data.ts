f6aefbea78978f6464c33a1755edd084832c0f55
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-29 22:15:26 +0700

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -40,6 +40,42 @@ const trendingItems = [
       "LEVTOP USB LED Strip Light RGB Waterproof Portable 5050 2M LED TV Backlight IP65 60Leds USB Powered Strip With 24keys Remote Controller",
     itemPrice: 100,
   },
+  {
+    itemImage:
+      "https://storage.googleapis.com/geniebook/images/bubblestore/product/614110_0_231010032313.19007090475332267",
+    itemName: "Fashion Zircon Alloy Butterfly Bracelet",
+    itemPrice: 402,
+    itemDiscount: {
+      before: 600,
+      after: 402,
+    },
+  },
+  {
+    itemImage:
+      "https://storage.googleapis.com/geniebook/images/bubblestore/product/614111_0_231010032530.10777343108134896",
+    itemName: "Sanrio Pochacco bell bracelet",
+    itemPrice: 509,
+    itemDiscount: {
+      before: 734,
+      after: 509,
+    },
+  },
+  {
+    itemImage:
+      "https://storage.googleapis.com/geniebook/images/bubblestore/product/615028_0_240520070428.9703798433229687",
+    itemName: "Feastables MrBeast Karl Gummies 50g",
+    itemPrice: 1638,
+  },
+  {
+    itemImage: "https://cf.shopee.sg/file/67d4ac21be7ad83bb34925dee252b69c",
+    itemName:
+      "Mini Face Humidifier Face Stream Beauty Spray Hand-held Water Machine USB Charging Nano Mist Spraying Manual Beauty Device",
+    itemPrice: 992,
+    itemDiscount: {
+      before: 992,
+      after: 992,
+    },
+  },
 ];
 
 const searchByCategories = [
@@ -127,6 +163,41 @@ const mostPopular = [
       before: 992,
       after: 992,
     },
+  },
+  {
+    itemImage:
+      "https://storage.googleapis.com/geniebook/images/bubblestore/product/614916_0_240422091325.7395534044457606",
+    itemName: "10kg Dumbbell Set",
+    itemPrice: 2233,
+    itemDiscount: {
+      before: 6049,
+      after: 2233,
+    },
+  },
+  {
+    itemImage:
+      "https://storage.googleapis.com/geniebook/images/bubblestore/product/597766_0_220506043059.1646124142780938",
+    itemName:
+      "5PCS 12 inch clear golden balloon for wedding birthday party balloon TM316",
+    itemPrice: 354,
+  },
+  {
+    itemImage:
+      "https://storage.googleapis.com/geniebook/images/bubblestore/product/613137_0_221130054020.3997252147203707",
+    itemName:
+      "Addition Subtraction Multiplication Division Stamp Practice Test Within 100 Educational Scrollable Math Toys",
+    itemPrice: 685,
+    itemDiscount: {
+      before: 1021,
+      after: 685,
+    },
+  },
+  {
+    itemImage:
+      "https://my-test-11.slatic.net/p/2b3276d16b9e6b4b1594ebf78046a4c7.jpg",
+    itemName:
+      "LEVTOP USB LED Strip Light RGB Waterproof Portable 5050 2M LED TV Backlight IP65 60Leds USB Powered Strip With 24keys Remote Controller",
+    itemPrice: 100,
   },
 ];
 
