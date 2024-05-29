0112f794dddc6879d30277f46595da2a724d2df1
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-29 19:47:19 +0700

    Update TopBar.tsx

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,17 +1,21 @@
 export function TopBar() {
   return (
-    <div className="flex size-full gap-4 p-3">
-      <img
-        className="p-1"
-        src="https://bubblestore.geniebook.com/_nuxt/img/logo-bubble-store.8b9a46f.svg"
-        alt="Bubble Store"
-      />
+    <div className="flex size-full items-center gap-4 p-4">
+      <a href="/">
+        <img
+          className="h-4 w-32"
+          src="https://bubblestore.geniebook.com/_nuxt/img/logo-bubble-store.8b9a46f.svg"
+          alt="Bubble Store"
+        />
+      </a>
       <span className="flex-1" />
       <img
+        className="size-6"
         src="https://bubblestore.geniebook.com/_nuxt/img/icon-search-white.765d2d5.svg"
         alt="Search"
       />
       <img
+        className="size-6"
         src="https://bubblestore.geniebook.com/_nuxt/img/Icon_Top Nav Bar_Cart.f96d8a4.svg"
         alt="Cart"
       />
