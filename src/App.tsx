8ea191a1a868e8e8af52f9852fefd6b956fa8e0a
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-29 19:55:51 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BottomBar } from "./components/BottomBar";
+import { Main } from "./components/Main";
+import { TopBar } from "./components/TopBar";
 
 function App() {
-  return <>App</>;
+  return (
+    <div className="bg-blue-50">
+      <div className="sticky top-0 z-10 h-12 bg-blue-900">
+        <TopBar />
+      </div>
+      <div className="h-fit min-h-screen">
+        <Main />
+      </div>
+      <div className="fixed inset-x-0 bottom-0 z-10 h-14 rounded-t-xl bg-white shadow-[0px_4px_12px]">
+        <BottomBar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
