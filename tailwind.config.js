0d03982dc28cf2450c9a6dc0c3eb64b1f44ef5a4
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-30 12:31:26 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: { sans: ['"Open Sans"'] },
+      fontFamily: { sans: ['"Open Sans Variable"'] },
     },
   },
   plugins: [],
